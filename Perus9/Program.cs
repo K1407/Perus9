8416ade82e1b61df2b628d6079f9df42d0e4133d
Hanna Harjula <K1407@student.jamk.fi>
2016-01-29 22:05:01 +0200

    Initial commit

---
+++ Perus9/Program.cs
@@ -0,0 +1,34 @@
+﻿// Perustehtävä 9
+
+using System;
+
+namespace Perus9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int sum = 0;
+            bool result;
+            do
+            {
+                Console.Write("Give a number ");
+                string line = Console.ReadLine();
+                result = Int32.TryParse(line, out number);
+                if (result)
+                {
+                    sum += number;
+                }
+                else
+                {
+                    Console.WriteLine("You didn't give a number.");
+                }
+            }
+            while (result && number != 0);
+
+            Console.WriteLine("The sum is " + sum);
+            Console.ReadLine();
+        }
+    }
+}
